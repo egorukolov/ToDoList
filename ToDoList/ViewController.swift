be8aab4940b8242f7a4a505dfb7a07368e8fb0c7
Egor Ukolov <ukolovegor@yandex.ru>
2020-07-02 12:50:56 +0700

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Egor Ukolov on 02.07.2020.
+//  Copyright © 2020 Egor Ukolov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
